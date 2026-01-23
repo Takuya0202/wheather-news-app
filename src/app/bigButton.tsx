6972f607926eb639cb0023ccbae8b1f0819fecd0
shion <shion48691811@gmail.com>
2026-01-23 21:08:43 +0900

    html Elementsからreact nativeに変更

--- src/app/bigButton.tsx
+++ src/app/bigButton.tsx
@@ -1,31 +1,30 @@
 import { Text, View, StyleSheet } from "react-native";
 import { VideoView, useVideoPlayer } from "expo-video";
-import { Div } from "@expo/html-elements";
 import BigFrame from "@/components/ui/bigFrame";
-import { cssInterop} from "nativewind";
-
-cssInterop(Div, {
-    className: "style",
-});
+import { cssInterop } from "nativewind";
 
 const bgWhetherVideo = require("@/assets/whether-mov/128879_960x540.mp4");
 
 export default function BigButton() {
-    const video = useVideoPlayer(bgWhetherVideo, (videoPlayer) => {
-        // ビデオをループ再生
-        videoPlayer.loop = true;
-        // 読み込まれたらビデオを再生
-        videoPlayer.play();
-        
-    });
+  const video = useVideoPlayer(bgWhetherVideo, (videoPlayer) => {
+    // ビデオをループ再生
+    videoPlayer.loop = true;
+    // 読み込まれたらビデオを再生
+    videoPlayer.play();
+  });
 
-    return (
-        <View className="relative flex-1">
-            {/* nativeControls={false}で動画のコントロールボタンを制御 */}
-            <VideoView player={video} contentFit="cover" nativeControls={false} style={[StyleSheet.absoluteFill, {width: "100%", height: "100%"}]}/>
-            <Div className="w-full h-full flex items-center justify-center">
-                <BigFrame/>
-            </Div>
-        </View>
-    );
-} 
+  return (
+    <View className="relative flex-1">
+      {/* nativeControls={false}で動画のコントロールボタンを制御 */}
+      <VideoView
+        player={video}
+        contentFit="cover"
+        nativeControls={false}
+        style={[StyleSheet.absoluteFill, { width: "100%", height: "100%" }]}
+      />
+      <View className="w-full h-full flex items-center justify-center">
+        <BigFrame />
+      </View>
+    </View>
+  );
+}
--- src/components/ui/bigFrame.tsx
+++ src/components/ui/bigFrame.tsx
@@ -2,15 +2,11 @@ import { GlassView } from "expo-glass-effect";
 import { Text, View } from "react-native";
 import { cssInterop } from "nativewind";
 import { Image } from "expo-image";
-import { Div } from "@expo/html-elements";
 
 cssInterop(GlassView, {
   className: "style",
 });
 cssInterop(Image, {
-  className: "style",
-});
-cssInterop(Div, {
   className: "style",
 });
 
@@ -26,24 +22,24 @@ export default function BigFrame() {
           source={require("@/assets/whether-img/27218923_1.png")}
           className="w-[130px] h-[130px]"
         />
-        <Div className="w-[235px] justify-between flex flex-row">
-          <Div className="flex flex-col ">
+        <View className="w-[235px] justify-between flex flex-row">
+          <View className="flex flex-col ">
             <Text className=" text-[16px] text-[#74C5FF]">最低</Text>
-            <Div className="flex flex-row items-baseline">
+            <View className="flex flex-row items-baseline">
               {/* 最低温度 */}
               <Text className="text-[36px] text-[#74C5FF] font-bold">13</Text>
               <Text className="text-[24px] text-[#74C5FF] font-bold">℃</Text>
-            </Div>
-          </Div>
-          <Div className="flex flex-col ">
+            </View>
+          </View>
+          <View className="flex flex-col ">
             <Text className=" text-[16px] text-[#E98383]">最高</Text>
-            <Div className="flex flex-row items-baseline">
-              {/* 最低温度 */}
+            <View className="flex flex-row items-baseline">
+              {/* 最高温度 */}
               <Text className="text-[36px] text-[#E98383] font-bold">13</Text>
               <Text className="text-[24px] text-[#E98383] font-bold">℃</Text>
-            </Div>
-          </Div>
-        </Div>
+            </View>
+          </View>
+        </View>
       </GlassView>
     </View>
   );
