f76a41c702f0a68fc95c35eae9ca674bd31f0a05
Takuya0202 <shimozono.t.24kdgn@gmail.com>
2026-01-27 13:59:24 +0900

    add

--- src/app/config.tsx
+++ src/app/config.tsx
@@ -1,8 +1,28 @@
-import { View, Text } from "react-native";
+import EditFrame from "@/components/ui/edit-frame";
+import { useVideoPlayer, VideoView } from "expo-video";
+import { View, StyleSheet } from "react-native";
+
+// 別の動画を使用（bigButton.tsxとは違う動画）
+const bgVideo = require("@/assets/whether-mov/132598_960x540.mp4");
 export default function Config() {
+  const video = useVideoPlayer(bgVideo, (videoPlayer) => {
+    videoPlayer.loop = true;
+    videoPlayer.play();
+  });
+
   return (
-    <View>
-      <Text>Config</Text>
+    <View className="relative flex-1">
+      <VideoView
+        player={video}
+        contentFit="cover"
+        nativeControls={false}
+        style={[StyleSheet.absoluteFill, { width: "100%", height: "100%" }]}
+      />
+      <View className="m-auto flex w-[94%] flex-1 flex-col items-start justify-center gap-3">
+        <EditFrame size="big" />
+        <EditFrame size="medium" />
+        <EditFrame size="small" />
+      </View>
     </View>
   );
 }
