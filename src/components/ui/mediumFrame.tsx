68108ab81a6bae8d63e6183c7a6296b0e93b58fe
shion <shion48691811@gmail.com>
2026-02-06 23:56:43 +0900

    scrollViewをindex.tsに入れ、全てのmediumframeを作成

--- src/components/ui/mediumFrame.tsx
+++ src/components/ui/mediumFrame.tsx
@@ -1,8 +1,14 @@
-// import { ScrollView } from "react-native-reanimated/lib/typescript/Animated";
-import { GlassView, View, StyleSheet } from "../index";
+import { GlassView, View, StyleSheet, ScrollView } from "../index";
 import React from "react";
 
 import Temperature from "../content/medium/temperature";
+import SensibleTemperature from "../content/medium/sensibleTemperature";
+import Clothing from "../content/medium/clothing";
+import Humidity from "../content/medium/humidity";
+import WindSpeed from "../content/medium/windSpeed";
+import PrecipitationProbability from "../content/medium/precipitationProbability";
+import Laundry from "../content/medium/laundry";
+import Stimulation from "../content/medium/stimulation";
 
 export default function MediumFrame() {
   const styles = StyleSheet.create({
@@ -10,24 +16,37 @@ export default function MediumFrame() {
       backgroundColor: "rgba(255, 255, 255, 0.2)",
     },
   });
+  const mediumFrameList = [
+    <Temperature />,
+    <SensibleTemperature />,
+    <Clothing />,
+    <Humidity />,
+    <WindSpeed />,
+    <PrecipitationProbability />,
+    <Laundry />,
+    <Stimulation />,
+  ];
 
   return (
-    <View className="flex h-[122px] w-width items-center rounded-[12px]">
-      {/* <ScrollView
+    <View className="flex h-full w-width items-center rounded-[12px]">
+      <ScrollView
         horizontal={false}
         showsHorizontalScrollIndicator={false}
         showsVerticalScrollIndicator={false}
         bounces={false}
         contentContainerClassName="items-center flex gap-4"
-      > */}
-      <GlassView
-        className="flex h-[122px] w-width items-center rounded-[12px]"
-        glassEffectStyle="clear"
-        style={styles.tintedGlassView}
       >
-        <Temperature />
-      </GlassView>
-      {/* </ScrollView> */}
+        {mediumFrameList.map((item, index) => (
+          <GlassView
+            key={index}
+            className="flex h-[122px] w-width items-center rounded-[12px]"
+            glassEffectStyle="clear"
+            style={styles.tintedGlassView}
+          >
+            {item}
+          </GlassView>
+        ))}
+      </ScrollView>
     </View>
   );
 }
