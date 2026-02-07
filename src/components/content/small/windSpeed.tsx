2bd8a20eee0dc008f42a1134283f122a975f1e7e
shion <shion48691811@gmail.com>
2026-02-07 13:24:43 +0900

    風速用のフレームを作成

---
+++ src/components/content/small/windSpeed.tsx
@@ -0,0 +1,20 @@
+import { Image, Text, View } from "@/components/index";
+
+export default function WindSpeed() {
+  return (
+    <View className="flex justify-between p-2">
+      <Text className="text-center font-notoSansJP text-[20px] text-[#000000]">風速</Text>
+      <View className="flex items-center justify-center">
+        <Image
+          source={require("@/assets/whether-img/windSpeed.png")}
+          className="h-[90px] w-[90px]"
+        />
+      </View>
+      <View className="flex flex-row items-baseline justify-center">
+        {/* 最高温度 */}
+        <Text className="notoSansJP text-[36px] font-bold text-[#000000]">13</Text>
+        <Text className="notoSansJP text-[24px] font-bold text-[#000000]">m</Text>
+      </View>
+    </View>
+  );
+}
