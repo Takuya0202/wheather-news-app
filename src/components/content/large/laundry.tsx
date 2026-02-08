67044f4aca41ef5e0a2f583f4ef0c50c204f10c6
shion <shion48691811@gmail.com>
2026-02-08 18:47:37 +0900

    気圧を実装

--- src/components/content/large/laundry.tsx
+++ src/components/content/large/laundry.tsx
@@ -20,6 +20,7 @@ export default function Laundry() {
       </View>
       <View className="flex w-[235px] flex-row items-center justify-between">
         <View className="flex flex-row items-baseline gap-2">
+          {/* 後にステータスごとに表示を切り替えるようにする */}
           <Image
             source={require("@/assets/whether-img/triangle.svg")}
             className="h-[55px] w-[55px]"
--- src/components/content/large/stimulation.tsx
+++ src/components/content/large/stimulation.tsx
@@ -1,6 +1,14 @@
 import { Image, Text, View } from "@/components/index";
+import { useEffect, useState } from "react";
+import atomos, { AtomosApi } from "@/lib/status/atomos";
 
 export default function Stimulation() {
+  const [data, setData] = useState<AtomosApi | null>(null);
+  useEffect(() => {
+    atomos().then((data) => {
+      setData(data);
+    });
+  }, []);
   return (
     <View>
       <Text className="text-center font-notoSansJP text-[28px] text-[#000000]">刺激</Text>
@@ -12,13 +20,14 @@ export default function Stimulation() {
       </View>
       <View className="flex w-[235px] flex-row items-center justify-between">
         <View className="flex flex-row items-baseline gap-2">
+          {/* 後にステータスごとに表示を切り替えるようにする */}
           <Image
             source={require("@/assets/whether-img/circle.svg")}
             className="h-[55px] w-[55px]"
             contentFit="contain"
           />
         </View>
-        <Text className="notoSansJP w-[110px] text-[24px] text-[#000000]">気圧が安定</Text>
+        <Text className="notoSansJP w-full px-3 text-[20px] text-[#000000]">{data?.message}</Text>
       </View>
     </View>
   );
