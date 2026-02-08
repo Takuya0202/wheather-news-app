67f026e319837e390bb2d4312b0d5bef0601995d
shion <shion48691811@gmail.com>
2026-02-08 18:18:07 +0900

    再生速度と背景の明るさを変更

--- src/app/bigButton.tsx
+++ src/app/bigButton.tsx
@@ -13,17 +13,18 @@ export default function BigButton() {
     videoPlayer.loop = true;
     // 読み込まれたらビデオを再生
     videoPlayer.play();
+    videoPlayer.playbackRate = 0.3;
   });
 
   return (
-    <View className="relative flex-1">
+    <View className="relative flex-1 bg-black/20">
       {/* nativeControls={false}で動画のコントロールボタンを制御 */}
       <VideoView
         player={video}
         contentFit="cover"
         nativeControls={false}
         pointerEvents="none"
-        style={[StyleSheet.absoluteFill, { width: "100%", height: "100%", opacity: 0.7 }]}
+        style={[StyleSheet.absoluteFill, { width: "100%", height: "100%", opacity: 0.6 }]}
       />
       <View className="flex h-full w-full flex-col items-center justify-center gap-4">
         <LargeFrame />
